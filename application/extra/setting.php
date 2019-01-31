e11c8e98721cccc28ec00e568d8b757a68fed605
Jason-cool <33286550+Jason-cool@users.noreply.github.com>
2019-02-01 00:06:19 +0800

    Update setting.php

--- application/extra/setting.php
+++ application/extra/setting.php
@@ -8,6 +8,6 @@
 
 return [
     // 'img_prfix' => 'http://z.cn/images',
-    'img_prfix' => 'http://120.79.68.162//images',
+    'img_prfix' => 'http://120.79.68.162/images',
     'token_expire_in'  => 7200  //有效时间
 ];
