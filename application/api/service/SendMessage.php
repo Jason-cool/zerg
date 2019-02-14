322543c0959a493f45737d3f80e152ca9ab9ed75
Jason-cool <33286550+Jason-cool@users.noreply.github.com>
2019-02-14 15:53:33 +0800

    Update SendMessage.php

--- application/api/service/SendMessage.php
+++ application/api/service/SendMessage.php
@@ -44,8 +44,6 @@ class SendMessage
             );
             $url = "https://api.weixin.qq.com/cgi-bin/message/wxopen/template/send?access_token={$accessToken}";
             $rtn = https_curl_json($url,$postData,'json');
-            print($rtn);
-            var_dump(json_decode($rtn,true));
 //            Log::write($rtn,'notice');
             if((json_decode($rtn,true))['errcode']==0){
                 orderModel::update(['id' => $order_id, 'status' => OrderStatusEnum::DELIVERED]);
