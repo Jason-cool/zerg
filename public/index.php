30c167693c8369461456a646e7310596c3db8011
Jason-cool <33286550+Jason-cool@users.noreply.github.com>
2019-02-14 19:22:12 +0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -16,7 +16,7 @@ define('APP_PATH', __DIR__ . '/../application/');
 define('LOG_PATH', __DIR__ . '/../log/');
 
 //定义配置文件目录
-define('CONF_PATH', __DIR__ . '/../conf/');
+//define('CONF_PATH', __DIR__ . '/../conf/');
 // 加载框架引导文件
 require __DIR__ . '/../thinkphp/start.php';
 
