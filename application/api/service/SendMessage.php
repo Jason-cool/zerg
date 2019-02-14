b8aa12132756489eb0a6d6581b5eff8a89b1f4fe
Jason-cool <33286550+Jason-cool@users.noreply.github.com>
2019-02-14 20:49:35 +0800

    Update SendMessage.php

--- application/api/service/SendMessage.php
+++ application/api/service/SendMessage.php
@@ -46,7 +46,7 @@ class SendMessage
             $rtn = https_curl_json($url,$postData,'json');
             Log::write($rtn,'notice');
             echo $rtn;
-            var_dump(json_decode($rtn,true));
+            var_dump(json_decode($rtn,true)['errcode']);
             return;
             //if((json_decode($rtn,true))['errcode']==0){
               //  orderModel::update(['id' => $order_id, 'status' => OrderStatusEnum::DELIVERED]);
